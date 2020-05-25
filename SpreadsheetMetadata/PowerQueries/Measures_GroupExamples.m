8a0dbda748a71d0be1ba894d6e9afb8185d06cf7
Charl Potgieter <charl.j.potgieter@gmail.com>
2020-05-25 21:27:36 +1000

    sundry

---
+++ SpreadsheetMetadata/PowerQueries/Measures_GroupExamples.m
@@ -0,0 +1,15 @@
+let
+
+    tbl = Table.FromRecords({
+        [NullHeader = ""]
+        }), 
+
+    ChangedType = Table.TransformColumnTypes(
+       tbl, 
+        {
+            {"NullHeader", type text}
+
+        })
+
+in
+    ChangedType
