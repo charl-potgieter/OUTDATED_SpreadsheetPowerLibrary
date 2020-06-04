e4f8aac214fb0c160bcf0b0827c1d88f0319fe9d
Charl Potgieter <charl.j.potgieter@gmail.com>
2020-06-04 21:58:20 +1000

    WIP Generalised Data Calculations

--- SpreadsheetMetadata/PowerQueries/DataTypes.m
+++ SpreadsheetMetadata/PowerQueries/DataTypes.m
@@ -1,11 +1,12 @@
 let
 
     tbl = Table.FromRecords({
-        [Data Type = "Balance Sheet", Time Class = "Point in time", Calculation method = "Point in time", Default = false], 
-        [Data Type = "Journals", Time Class = "Time Period", Calculation method = "Sum of transactions", Default = false], 
-        [Data Type = "P&L (from tb)", Time Class = "Time Period", Calculation method = "Difference between points in time", Default = true], 
-        [Data Type = "P&L (from jnls)", Time Class = "Time Period", Calculation method = "Sum of transactions", Default = false], 
-        [Data Type = "Trial Balance", Time Class = "Point in time", Calculation method = "Point in time", Default = false]
+        [Data Type = "Balance Sheet", Time Class = "As at month end", Calculation method = "Point in time", Minimum time granularity = "Month", Default = false], 
+        [Data Type = "Journals", Time Class = "Time Period", Calculation method = "Sum of transactions", Minimum time granularity = "Month", Default = false], 
+        [Data Type = "P&L (from tb)", Time Class = "Time Period", Calculation method = "Difference between points in time", Minimum time granularity = "Month", Default = true], 
+        [Data Type = "P&L (from jnls)", Time Class = "Time Period", Calculation method = "Sum of transactions", Minimum time granularity = "Month", Default = false], 
+        [Data Type = "Trial Balance", Time Class = "As at month end", Calculation method = "Point in time", Minimum time granularity = "Month", Default = false],
+        [Data Type = "Customer Count", Time Class = "Time Period", Calculation method = "Sum of transactions", Minimum time granularity = "Date", Default = false]
         }), 
 
     ChangedType = Table.TransformColumnTypes(
@@ -13,7 +14,8 @@ let
         {
             {"Data Type", type text},
             {"Time Class", type text},  
-            {"Calculation method", type text},  
+            {"Calculation method", type text},
+            {"Minimum time granularity", type text},
             {"Default", type logical}
 
         }),
--- SpreadsheetMetadata/PowerQueries/TimePeriods.m
+++ SpreadsheetMetadata/PowerQueries/TimePeriods.m
@@ -6,7 +6,8 @@ let
         [Time Period = "YTD", Time Period Sort By Col = "3"], 
         [Time Period = "PY", Time Period Sort By Col = "4"], 
         [Time Period = "Total", Time Period Sort By Col = "5"], 
-        [Time Period = "Point in time", Time Period Sort By Col = "6"]
+        [Time Period = "As at date", Time Period Sort By Col = "6"],
+        [Time Period = "As at month end", Time Period Sort By Col = "7"]
         }), 
 
     ChangedType = Table.TransformColumnTypes(
