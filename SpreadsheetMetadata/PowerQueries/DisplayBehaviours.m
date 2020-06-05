18015d2b0c7e11d1771dba144c81f7118899dbb8
Charl Potgieter <charl.j.potgieter@gmail.com>
2020-06-05 21:28:23 +1000

    Before winding back generalisations

--- SpreadsheetMetadata/PowerQueries/DisplayBehaviours.m
+++ SpreadsheetMetadata/PowerQueries/DisplayBehaviours.m
@@ -1,17 +1,20 @@
 let
 
     tbl = Table.FromRecords({
-        [DisplayBehaviour = "Display QTD at non-qtr end point"], 
-        [DisplayBehaviour = "Display YTD at non-year end point"], 
-        [DisplayBehaviour = "Display QTD at max month for qtr displayed only"], 
-        [DisplayBehaviour = "Display YTD for max month of year displayed only"],
-        [DisplayBehaviour = "Restrict hierarchy levels"]
+        [DisplayBehaviour = "Display MTD at non-month end date", SortByCol = 1], 
+        [DisplayBehaviour = "Display QTD at non-qtr end point", SortByCol = 2], 
+        [DisplayBehaviour = "Display YTD at non-year end point", SortByCol = 3], 
+        [DisplayBehaviour = "Display MTD at max day for month displayed only", SortByCol = 4], 
+        [DisplayBehaviour = "Display QTD at max month for qtr displayed only", SortByCol = 5], 
+        [DisplayBehaviour = "Display YTD for max month of year displayed only", SortByCol = 6],
+        [DisplayBehaviour = "Restrict hierarchy levels", SortByCol = 7]
         }), 
 
     ChangedType = Table.TransformColumnTypes(
        tbl, 
         {
-            {"DisplayBehaviour", type text}
+            {"DisplayBehaviour", type text}, 
+            {"SortByCol", Int64.Type}
 
         })
 
